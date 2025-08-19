c74a0387eee59cc63771b4b42846af5aada75b72
Suraj-78725 <surajbansode2004@gmail.com>
2025-08-19 14:21:38 +0530

    dependency configuration completed

---
+++ src/main/java/com/nimap/springboot_category_product_crud/SpringbootCategoryProductCrudApplication.java
@@ -0,0 +1,13 @@
+package com.nimap.springboot_category_product_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCategoryProductCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCategoryProductCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nimap/springboot_category_product_crud/SpringbootCategoryProductCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nimap.springboot_category_product_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCategoryProductCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
